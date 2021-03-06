2e31b954814d1a7d1b3f20f9feb8d50756120d0c
Cosmic Flamingo <acosmicflamingo@gmail.com>
2021-03-06 10:02:44 -0600

    Attempt at suppressing known error

--- Demo/Untouched/Demo/ViewController.swift
+++ Demo/Untouched/Demo/ViewController.swift
@@ -8,16 +8,9 @@ class ViewController: UIViewController {
         CFLocalizedString("How are you?", comment: "")
         CFLocalizedString("I'm fine", comment: "Comment for I'm fine - #bc-ignore!")
 
-        title = BartyCrouch.translate(key: "onboarding.first-page.header-title", translations: [.english: "Page Title", .german: "Seitentitel"])
-        let lines: Int = (0 ..< 10).map { "\($0 + 1): \(BartyCrouch.translate(key: "onboarding.first-page.line", translations: [:], comment: "Line Comment"))" }.count
+        title = CFLocalizedString("onboarding.first-page.header-title", comment: "")
+        let lines: Int = (0 ..< 10).map { "\($0 + 1): \(CFLocalizedString("onboarding.first-page.line", comment: "Line Comment"))" }.count
 
-        BartyCrouch
-            .translate(
-                key : "ShortKey",
-                translations : [
-                    BartyCrouch.SupportedLanguage.english :
-                    "Some Translation"
-                ]
-        )
+        CFLocalizedString("ShortKey", comment: "")
     }
 }
